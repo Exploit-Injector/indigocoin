4c24c74208f52a6be0e95fb72f4da42a414465a2
Feedbug77 <55884881+Exploit-Injector@users.noreply.github.com>
2020-04-09 14:08:23 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?
+// Ambil User Agent dari config sc Mastercoin
+// Buat yang belum pernah daftar. Capture dulu ambil User Agentnya agar tidak system tidak mendetek penyalahgunaan
+
+$UserAgent = "xxxxxx";
+$Email = "xxxxx";
+$Password = "xxxxxxx";
